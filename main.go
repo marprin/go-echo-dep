c217629cca8e96b6cd9400ea55f38dc08d91a506
marprin <marprin.hennes@acommerce.asia>
2018-09-21 18:32:47 +0700

    Setup logger and middleware

--- main.go
+++ main.go
@@ -4,12 +4,32 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/labstack/gommon/log"
 )
 
 func main() {
 	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Serve any static file
+	e.Static("/static", "assets")
+
+	// Serve robots.txt file
+	e.File("/robots.txt", "assets/robots.txt")
+
+	// Customize Logger
+	if l, ok := e.Logger.(*log.Logger); ok {
+		l.SetHeader("${time_rfc3339} ${level}")
+	}
+
 	e.GET("/", func(c echo.Context) error { 
 		return c.String(http.StatusOK, "Hello, world")
 	})
+
+	// Start Server
 	e.Logger.Fatal(e.Start(":7600"))
 }
